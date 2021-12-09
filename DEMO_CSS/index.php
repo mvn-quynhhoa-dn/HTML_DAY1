b20b72b2e003d68dd85b44e74b02b6412e465975
mvn-quynhhoa-dn <94818581+mvn-quynhhoa-dn@users.noreply.github.com>
2021-12-09 09:44:30 +0700

    css

---
+++ DEMO_CSS/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="main">
+         <!--  Child Selector --> 
+         <div>
+   <h1 class="header" id="header-tiltle">hello chao cac ban</h1>
+         </div>
+   <div  >
+   <h2>asrawrfa</h2>
+   <ul class="row header-menu" id="menu">
+            <li><a href="#">Menu 1</a></li>
+            <li><a href="#">Menu 2</a>
+    <ul id="sub">             
+            <li><a href="#">Sub Menu 1 </a></li>
+            <li><a href="#">Sub Menu 2</a></li>
+            <li><a href="#">Sub Menu 3</a></li>
+    </ul>
+        </li>
+        <li><a href="#">Menu 3</a></li>
+        <li><a href="#">Menu 4</a></li>
+        <li><a href="#">Menu 5</a></li>
+    </ul>
+    </div>
+    <input type="text">
+   <h2 class="top-title">đây là phần Attribute Selector</h2>
+   <p><a class="highlight" href="#">CSS Syntax</a></p>
+
+   <p>Tôi tên là <q lang="no">peter</q> Some text.</p>
+   
+   <a><img src="https://thuthuatnhanh.com/wp-content/uploads/2021/06/hinh-anh-con-soi-1.jpg"; style="width:50%;" ></a>
+   <div class="noidung">This text is the content of the box. We have added a
+        50px padding, 20px margin and a 15px green border.
+     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+        .</div>
+    </div>
+</body>
+</html>
